1e69a1a30b0b2d301a3a476e1fc63e39bcd3d83a
anin27 <aninthomas27@gmail.com>
2026-04-12 16:32:22 +0100

    Added Flask routes for training events and registrations pages

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
-    return "Flask app is running"
+def training_events_page():
+    return render_template("training_events.html")
+
+
+@app.route("/registrations-page")
+def registrations_page():
+    return render_template("registrations.html")
 
 
 if __name__ == "__main__":
