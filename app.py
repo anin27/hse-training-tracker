db64f583ed620be107402e39bd73571265b13739
anin27 <aninthomas27@gmail.com>
2026-04-12 16:30:09 +0100

    Create basic Flask application setup

--- app.py
+++ app.py
@@ -2,24 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "App is running"
+    return "Flask app is running"
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def training_page():
-    return render_template("training_events.html")
-
-@app.route("/registrations-page")
-def registrations_page():
-    return render_template("registrations.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
